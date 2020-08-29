471e494ae46fc465b4fb63b245fb246483d94bfb
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-29 07:46:49 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -245,49 +245,14 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
     LOGGER.info(thumbnail_location)
     #
     if UPLOAD_AS_DOC.upper() == 'TRUE':
+        thumb = None
+        thumb_image_path = None
+        if os.path.exists(thumbnail_location):
+        	thumb_image_path = await copy_file(thumbnail_location, os.path.dirname(os.path.abspath(local_file_name)))
+        	thumb = thumb_image_path
         message_for_progress_display = message
         if not edit_media:
             message_for_progress_display = await message.reply_text("starting upload of {}".format(os.path.basename(local_file_name)))
-        if local_file_name.upper().endswith(("MKV", "MP4", "WEBM")):
-                metadata = extractMetadata(createParser(local_file_name))
-                duration = 0
-                if metadata.has("duration"):
-                	duration = metadata.get('duration').seconds
-                width = 0
-                height = 0
-                thumb_image_path = None
-                if os.path.exists(thumbnail_location):
-                    thumb_image_path = await copy_file(
-                        thumbnail_location,
-                        os.path.dirname(os.path.abspath(local_file_name))
-                    )
-                else:
-                    thumb_image_path = await take_screen_shot(
-                        local_file_name,
-                        os.path.dirname(os.path.abspath(local_file_name)),
-                        (duration / 2)
-                    )
-                    # get the correct width, height, and duration for videos greater than 10MB
-                    if os.path.exists(thumb_image_path):
-                        metadata = extractMetadata(createParser(thumb_image_path))
-                        if metadata.has("width"):
-                            width = metadata.get("width")
-                        if metadata.has("height"):
-                            height = metadata.get("height")
-                        # ref: https://t.me/PyrogramChat/44663
-                        # https://stackoverflow.com/a/21669827/4723940
-                        Image.open(thumb_image_path).convert(
-                            "RGB"
-                        ).save(thumb_image_path)
-                        img = Image.open(thumb_image_path)
-                        # https://stackoverflow.com/a/37631799/4723940
-                        img.resize((320, height))
-                        img.save(thumb_image_path, "JPEG")
-                        # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-                #
-                thumb = None
-                if thumb_image_path is not None and os.path.isfile(thumb_image_path):
-                    thumb = thumb_image_path
         sent_message = await message.reply_document(
             document=local_file_name,
     	    # quote=True,
@@ -307,6 +272,8 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
         if message.message_id != message_for_progress_display.message_id:
             await message_for_progress_display.delete()
         os.remove(local_file_name)
+        if thumb is not None:
+        	os.remove(thumb)
     else:
         try:
             message_for_progress_display = message
